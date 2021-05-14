b491941351087ce0fb32e389997c26d418656010
busitaro <busitaro10@gmail.com>
2021-05-14 18:43:16 +0900

    improve: 添付ファイル保存の際、同名ファイルがあった場合に別名で保存するように挙動変更
    mail.py
       添付ファイル保存前に、make_store_name()によって、同名ファイルの存在をチェックし、
       同名のファイルがあった場合に、末尾に連番を付与する処理を追加
       添付ファイルを上書きしないように修正

--- get_mail/mail.py
+++ get_mail/mail.py
@@ -1,5 +1,6 @@
 from O365 import Account, FileSystemTokenBackend
 from os import makedirs
+from os.path import isfile
 
 from .config.config_api import Config
 from .run_time import RunTime
@@ -75,4 +76,37 @@ def store_attachment_file(message, path: str):
     path: 保存先
     """
     for attachment in message.attachments:
-        attachment.save(path)
+        attachment.save(path, custom_name=make_store_name(attachment.name, path))
+
+
+def make_store_name(attachment_name: str, path: str):
+    """
+    添付ファイルの保存名を作成する
+
+    Params
+    ------
+    attachment_name: str
+        添付ファイル名
+    path: str
+        保存先パス
+
+    Returns
+    -------
+    0: 保存ファイル名
+    """
+    if isfile('{}/{}'.format(path, attachment_name)):
+        # 同名ファイルが存在した場合
+
+        # ファイル名の後ろに "_連番" を付与する
+        underbar_split = attachment_name.split('_')
+        base_name, suffix_num = '_'.join(underbar_split[:-1]), underbar_split[-1]
+
+        # _連番を付けた上で、再度同名ファイルの存在チェック
+        if suffix_num.isdecimal():
+            suffix_num = str(int(suffix_num) + 1)
+        else:
+            suffix_num = '1'
+        store_name = make_store_name('{}_{}'.format(base_name, suffix_num), path)
+    else:
+        store_name = attachment_name
+    return store_name
