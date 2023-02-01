3b8bd46e8d7bf8431a700a6fec252761241182d1
busitaro <busitaro10@gmail.com>
2023-02-01 17:28:18 +0900

    :zap: Monitoringオブジェクトの各プロパティについて、初期値を設定

--- get_mail/data/monitoring.py
+++ get_mail/data/monitoring.py
@@ -7,17 +7,25 @@ class Monitoring:
     件名を検索するワード
     このワードが件名に含まれるメールが対象となる
     """
-    search_word: str
+    search_word: str = ''
     """
     検索対象のディレクトリ
     階層をリストで表現する
     """
-    search_directory: list
+    search_directory: list = dataclasses.field(default_factory=list)
     """
     添付ファイル出力先ディレクトリ
     """
-    file_out_dir: str
+    file_out_dir: str = ''
     """
     メール検索間隔(秒)
     """
-    period: int
+    period: int = -1
+    """
+    送信メール件名
+    """
+    send_subject: str = ''
+    """
+    送信メール内容
+    """
+    send_body: str = ''
